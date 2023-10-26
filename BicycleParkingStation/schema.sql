d148775485d3bf33bae719383caa085a37819f7b
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:13:18 +0200

    test of SQL export

--- BicycleParkingStation/schema.sql
+++ BicycleParkingStation/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model BicycleParkingStation of the subject dataModel.OSLO for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE BicycleParkingStation_type AS ENUM ('BicycleParkingStation');
-CREATE TABLE BicycleParkingStation (InfrastructureElement.geometry JSON, ParkingFacility.capacity JSON, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type BicycleParkingStation_type);
+CREATE TABLE BicycleParkingStation (InfrastructureElement.geometry JSON, ParkingFacility.capacity JSON, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type BicycleParkingStation_type);
