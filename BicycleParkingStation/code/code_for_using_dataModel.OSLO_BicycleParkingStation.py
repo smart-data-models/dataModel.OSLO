d63688108b52bba24822afa17e81d156bd8f5761
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:42:30 +0200

    created example of code

--- BicycleParkingStation/code/code_for_using_dataModel.OSLO_BicycleParkingStation.py
+++ BicycleParkingStation/code/code_for_using_dataModel.OSLO_BicycleParkingStation.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "BicycleParkingStation"
 subject = "dataModel.OSLO"
-ParkingFacility.capacity = {'type': 'Property', 'value': {'type': 'Capacity', 'Capacity.total': {'type': 'Property', 'value': 20}}}
+ParkingFacility.capacity = {'type': 'Capacity', 'Capacity.total': 20}
 attribute = "ParkingFacility.capacity"
 value = ParkingFacility.capacity
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-InfrastructureElement.geometry = {'type': 'Property', 'value': {'type': 'Geometry', 'Geometry.wkt': {'type': 'Property', 'value': 'POINT(3.313743000000 50.855703000000)'}}}
+InfrastructureElement.geometry = {'type': 'Geometry', 'Geometry.asWkt': 'POINT(3.313743000000 50.855703000000)'}
 attribute = "InfrastructureElement.geometry"
 value = InfrastructureElement.geometry
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
